4d5527a93d01bb2de92eff278e83cfd872d7513f
Anton Panov <hightemp.unknown@gmail.com>
2019-06-22 18:26:17 +0300

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 
 mkdir /etc/renice_processes_service/
 cp renice_processes_service.cfg /etc/renice_processes_service/
-cp renice_processes_service.service 
+cp renice_processes_service.service /usr/lib/systemd/system
+cp renice_processes_service /usr/bin/renice_processes_service
