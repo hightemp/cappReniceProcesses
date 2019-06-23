6c3cad398fde5c9932180e03637f765bf49f3816
Anton Panov <hightemp.unknown@gmail.com>
2019-06-23 22:35:59 +0300

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 mkdir /etc/renice_processes_service/
-cp -f renice_processes_service.cfg /etc/renice_processes_service/
-cp -f renice_processes_service.service /usr/lib/systemd/system
-cp -f renice_processes_service /usr/bin/renice_processes_service
+yes | cp -f renice_processes_service.cfg /etc/renice_processes_service/
+yes | cp -f renice_processes_service.service /usr/lib/systemd/system
+yes | cp -f renice_processes_service /usr/bin/renice_processes_service
 systemctl daemon-reload
 systemctl enable renice_processes_service
 systemctl start renice_processes_service
